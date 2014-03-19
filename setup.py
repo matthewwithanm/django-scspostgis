d777cc1f67057c89866c241054cd92d9d84d7fdc
Matthew Dapena-Tretter <m@tthewwithanm.com>
2014-03-19 09:40:51 -0400

    PEP8 fixes

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 import os
 from distutils.core import setup
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 README = read('README.markdown')
 
 setup(
-    name = 'django-scspostgis',
-    version = '0.1',
+    name='django-scspostgis',
+    version='0.1',
     description='A PostGIS db backend that works with standard_conforming_strings.',
     long_description=README,
 
-    author = 'Matthew Tretter',
-    author_email = 'matthew@exanimo.com',
-    packages = [
+    author='Matthew Tretter',
+    author_email='matthew@exanimo.com',
+    packages=[
         'scspostgis',
     ],
-    install_requires = [
-    ],
-    classifiers = [
+    install_requires=[],
+    classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
